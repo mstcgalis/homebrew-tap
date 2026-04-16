2ab74031fc729a71e4c92da9dc7dda264817b345
Daniel Gális <danielgalis21@gmail.com>
2026-04-16 16:56:24 +0200

    Bump lufs to v1.2.4

--- Formula/lufs.rb
+++ Formula/lufs.rb
@@ -1,8 +1,8 @@
 class Lufs < Formula
   desc "LUFS analysis and track comparison for .wav files"
   homepage "https://github.com/mstcgalis/lufs"
-  url "https://github.com/mstcgalis/lufs/archive/refs/tags/v1.2.3.tar.gz"
-  sha256 "d58f265cf45f61d5bb7e3cf486da9fc3560e3a5c001056deefdb16c9a78b834d"
+  url "https://github.com/mstcgalis/lufs/archive/refs/tags/v1.2.4.tar.gz"
+  sha256 "c3c52c50a5eaff1c065b3777e34cde56ed9347af0b402b002d3b2240437869a8"
   license "MIT"
 
   depends_on "ffmpeg"
